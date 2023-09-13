925b5246ef08669b9b011a4a9a98912f7bc215fd
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-09-13 23:36:57 +0200

    Main Window (part)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rosetta {
+    using Class;
+    using System.Windows.Forms;
+    using TrickyUnits;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void BT_NewProject_Click(object sender, RoutedEventArgs e) {
+            var NewProject = FFS.RequestFile(true);
+            if (NewProject == "") return;
+            var NPK = qstr.StripDir(NewProject);
+            if (ProjectList.PRJ.ContainsKey(NPK)) {
+                Confirm.Annoy("Duplicate project name!\nPlease select another", "Error", System.Windows.Forms.MessageBoxIcon.Error);
+                return;
+            }
+            ProjectList.Create(LB_Projects, NewProject);
+        }
+    }
+}
