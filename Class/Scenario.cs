a0ccd70ab0a534c6c355ec2496346be23e6db3c3
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-09-17 15:58:10 +0200

    Basis database further worked out (Temporary workspace. Note the earlier two commits were from that workspace too, but I forgot to note)

--- Class/Scenario.cs
+++ Class/Scenario.cs
@@ -52,16 +52,86 @@ namespace Rosetta.Class {
 		}
 
 		internal class CTag {
-			readonly CEntry Parent = null;
-			readonly string Tag = "";
+			internal GINIE Data => Parent.Data;
+			readonly internal CEntry Parent = null;
+			readonly internal string Tag = "";
 			internal CTag (CEntry _Parent,string _Tag) {
 				Parent = _Parent;
 				Tag = _Tag;
 				Parent.Tags[Tag] = this;
 			}
+			internal List<CPage> Page = new List<CPage>();
 
 			internal ProjectData Project => Parent.Parent.Parent;
 
+		}
+
+		internal class CPage {
+			internal readonly CTag Parent = null;
+			internal GINIE Data => Parent.Parent.Data;
+
+			string CGCat => $"::CENTRAL::{Parent.Tag}::{PageIndex}::";
+
+			internal int PageIndex {
+				get {
+					for (int i = 0; i < Parent.Page.Count; ++i) if (Parent.Page[i] == this) return i;
+					return -1;
+				}
+			}
+			internal string PicDir {
+				get => Data[CGCat, "PicDir"];
+				set => Data[CGCat, "PicDir"] = value;
+			}
+
+			internal string PicSpecific {
+				get => Data[CGCat, "PicSpecific"];
+				set => Data[CGCat, "PicSpecific"] = value;
+			}
+
+			internal bool NameLinking {
+				get => Data[CGCat, "NameLinking"].ToLower() == "true";
+				set => Data[CGCat, "NameLinking"] = value.ToString();
+			}
+
+			Dictionary<string, CSLang> _Lang = new Dictionary<string, CSLang>();
+
+		}
+		
+		class CSLang {
+			internal readonly CPage Parent = null;
+			internal readonly string Lang = "";
+			internal GINIE Data => Parent.Parent.Data;
+			internal CSLang(CPage _Parent,string _Lang) { Parent= _Parent; Lang = _Lang; }
+			internal int PageIndex => Parent.PageIndex;
+
+			internal string CGLCat => $"::LANG::{Lang}::{Parent.Parent.Tag}::{PageIndex}::";
+
+			internal string Header {
+				get {
+					if (Parent.NameLinking) return Parent.PicDir;
+					return Data[CGLCat, "Header"];
+				}
+				set {
+					if (Parent.NameLinking) Parent.PicDir = value;
+					else Data[CGLCat, "Header"] = value;
+				}
+			}
+			internal string Content {
+				get {
+					var ret = new StringBuilder();
+					var lst = Data.List(CGLCat, "Content");
+					for(int i = 0; i < lst.Count; ++i) {
+						if (i > 0) ret.Append("\n");
+						ret.Append(lst[i]);
+					}
+					return ret.ToString();
+				}
+				set {
+					var v = value.Replace("\r", "");
+					var lst = Data.List(CGLCat, "Content"); lst.Clear();
+					foreach(var l in v.Split('\n')) lst.Add(l);
+				}
+			}
 		}
 
 		
