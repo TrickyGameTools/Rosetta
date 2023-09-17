f2086461069b90f2e2bb10732efe6f0e1fe3c7c2
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-09-17 14:12:11 +0200

    Base for Export (needed later) (Temporary Workspace)

---
+++ Export/XBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rosetta.Class;
+
+namespace Rosetta.Export {
+	abstract internal class XBase {
+		abstract internal void Export(ProjectData D, string language);
+
+		readonly static internal SortedDictionary<string,XBase> Register = new SortedDictionary<string,XBase>();
+	}
+}
