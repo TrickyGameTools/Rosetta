156fc1bf987b822f149082c6cea2a99b1d60a33e
Jeroen P. Broks <facebook@tbbs.nl>
2025-10-06 21:27:21 +0200

    Project Update

--- src/Class/Scenario.hpp
+++ src/Class/Scenario.hpp
@@ -1,27 +1,27 @@
 // License:
-// 
+//
 // Rosetta
 // Scenario Classes (header)
-// 
-// 
-// 
+//
+//
+//
 // 	(c) Jeroen P. Broks, 2023, 2025
-// 
+//
 // 		This program is free software: you can redistribute it and/or modify
 // 		it under the terms of the GNU General Public License as published by
 // 		the Free Software Foundation, either version 3 of the License, or
 // 		(at your option) any later version.
-// 
+//
 // 		This program is distributed in the hope that it will be useful,
 // 		but WITHOUT ANY WARRANTY; without even the implied warranty of
 // 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // 		GNU General Public License for more details.
 // 		You should have received a copy of the GNU General Public License
 // 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // 	Please note that some references to data like pictures or audio, do not automatically
 // 	fall under this licenses. Mostly this is noted in the respective files.
-// 
+//
 // Version: 25.10.06 XXXIII
 // End License
 
@@ -281,7 +281,7 @@ namespace Slyvina { namespace Rosetta { namespace Class {
 			std::map<String,CEntry> Entries{};
 			_CScenario(_ProjectData* _Parent) { Parent = _Parent; }
 
-			inline void SaveMe(bool force=false);
+			void SaveMe(bool force=false);
 
 			ProjectData CurrentProject();
 
